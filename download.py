ad08624fff0d94d27db7c0229c2e288d2c70ac1a
mamaamahustla-cpu <mamaamahustla@gmail.com>
2026-02-26 08:49:54 +0300

    Add files via upload

---
+++ download.py
@@ -0,0 +1,8 @@
+import asyncio
+import random
+import re
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.enums import ParseMode
+
